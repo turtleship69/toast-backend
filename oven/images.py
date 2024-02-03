7de6ce5bfc9115e3d78e2cc57b551992eb5f3cae
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-02-03 22:34:24 +0000

    finish line lol

--- oven/images.py
+++ oven/images.py
@@ -1,10 +1,15 @@
 # blueprint to serve images from /content/images
 #THIS SHOULD ONLY BE USED AS A FALLBACK - USE A PROPER CDN
-from flask import Blueprint, send_from_directory
+from flask import Blueprint, make_response, send_from_directory
+from flask import make_response
 
 
 bp = Blueprint('images', __name__, url_prefix="images")
 
 @bp.route('/<path:filename>')
 def serve_image(filename):
-    return send_from_directory('images', filename)
+    image = make_response(send_from_directory('images', filename))
+    image.headers['Cache-Control'] = 'public, max-age=604800' # one week
+    image.headers['Netlify-CDN-Cache-Control'] = 'public, max-age=604800' # one week
+    image.headers['Content-Type'] = 'image/jpeg'
+    return image
