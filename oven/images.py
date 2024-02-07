712b0067d9f48eed5fc930f4626188177e8cdbf7
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-02-07 19:06:02 +0000

    add headers

--- oven/images.py
+++ oven/images.py
@@ -8,7 +8,7 @@ bp = Blueprint('images', __name__, url_prefix="/images")
 
 @bp.route('/<path:filename>')
 def serve_image(filename):
-    image = send_from_directory(path.join(getcwd(), 'content/images'), filename)
+    image = make_response(send_from_directory(path.join(getcwd(), 'content/images'), filename))
     image.headers['Cache-Control'] = 'public, max-age=604800' # one week
     image.headers['Netlify-CDN-Cache-Control'] = 'public, max-age=604800' # one week
     image.headers['Content-Type'] = 'image/jpeg'
