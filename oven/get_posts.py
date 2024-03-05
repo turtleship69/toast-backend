906e982635f141deff5a0ade729390ebb0a02813
Nathan Machane <73017570+turtleship69@users.noreply.github.com>
2024-03-05 15:01:44 +0000

    add home page

--- oven/get_posts.py
+++ oven/get_posts.py
@@ -1,20 +1,20 @@
-from .models import getPostById
+from .models import Post, getPostById
 
-from flask import Blueprint, g
-from flask import jsonify
+from flask import Blueprint, g, jsonify
 import re
 
 bp = Blueprint("get_posts", __name__, url_prefix="/get_posts")
 
 POST_ID_FORMAT = re.compile(
-        "^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z",
-        re.I,
-    )
+    "^[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}\Z",
+    re.I,
+)
 
 
 # 0 = Only Me
 # 1 = Only Friends
 # 2 = Everyone
+
 
 @bp.route("/get_post/<post_id>")
 # @login_required
@@ -35,19 +35,27 @@ def get_post(post_id):
 
     if not post:
         return jsonify({"status": "error", "message": "Post not found"}), 400
-    
+
     if post.visibility == 2:
         return jsonify(post.getDict())
-    else: 
+    else:
         if not g.logged_in:
-            return jsonify({"status": "error", "message": "Not logged in", "errorCode": "loginRequired"}), 400
+            return (
+                jsonify(
+                    {
+                        "status": "error",
+                        "message": "Not logged in",
+                        "errorCode": "loginRequired",
+                    }
+                ),
+                400,
+            )
     print(g.User.username)
-    
-    
-    if post.visibility == 0: 
+
+    if post.visibility == 0:
         if g.UserID == post.poster_id:
             return jsonify(post.getDict())
-        else: 
+        else:
             return jsonify({"status": "error", "message": "Post not found"}), 400
 
     # check if post is public
@@ -67,3 +75,64 @@ def get_post(post_id):
     post_dict = post.getDict()
 
     return jsonify(post_dict)
+
+
+@bp.route("/home")
+def home():
+    if not g.logged_in:
+        # get 10 most recent public posts
+        posts = g.db.execute(
+            "SELECT lp.*, u.Username, u.GravatarURL FROM live_posts lp JOIN Users u ON lp.UserID = u.UserID WHERE lp.Visibility = 2 ORDER BY lp.UploadTime DESC LIMIT 10"
+        ).fetchall()
+    else:
+        # find all people g.User follows (following type=1 or (type=2 and accepted=true))
+        following = g.db.execute(
+            "SELECT Followee FROM followers WHERE Follower = ? AND Type = 1",
+            (g.UserID,),
+        ).fetchall()
+
+        following = [x[0] for x in following] if following else [None]
+        print(following)
+
+        friends = g.db.execute(
+            "SELECT Followee FROM followers WHERE Follower = ? AND Type = 2 AND Accepted = 1",
+            (g.UserID,),
+        ).fetchall()
+        friends = [x[0] for x in friends] if friends else [None]
+        print(friends)
+
+        # get 10 most recent posts from:
+        # people g.User follows - only public posts
+        # people g.User is friends with - public and friends only posts
+        # posts by g.User - all posts
+        command = f"SELECT lp.*, u.Username, u.GravatarURL FROM live_posts lp JOIN Users u ON lp.UserID = u.UserID WHERE (lp.Visibility = 2 AND lp.UserID IN ({','.join('?'*len(following))})) OR (lp.Visibility >= 1 AND lp.UserID IN ({','.join('?'*len(friends))})) OR lp.UserID = ? ORDER BY lp.UploadTime DESC LIMIT 10".format()
+        print(command)
+        posts = g.db.execute(
+            command,
+            (*following,
+            *friends,
+            g.UserID,)
+        ).fetchall()
+
+    # convert posts to Post objects
+    postsDict = {"posts": []}
+    for post in posts:
+        postsDict["posts"].append(
+            Post(
+                post[0],
+                post[1],
+                post[11],
+                post[12],
+                post[2],
+                post[3],
+                post[4],
+                post[5],
+                post[6],
+                post[7],
+                post[8],
+                post[9],
+                post[10],
+            ).getDict()
+        )
+
+    return jsonify(postsDict)
